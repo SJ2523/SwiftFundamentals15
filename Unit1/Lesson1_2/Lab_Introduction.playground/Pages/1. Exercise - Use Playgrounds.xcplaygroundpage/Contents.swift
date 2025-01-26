6b55b95bba57c618482de91f4086a36755e40c90
SJ2523 <lees2523@durhamtech.edu>
2025-01-26 13:38:28 -0500

    Update Contents.swift

--- Unit1/Lesson1_2/Lab_Introduction.playground/Pages/1. Exercise - Use Playgrounds.xcplaygroundpage/Contents.swift
+++ Unit1/Lesson1_2/Lab_Introduction.playground/Pages/1. Exercise - Use Playgrounds.xcplaygroundpage/Contents.swift
@@ -10,13 +10,18 @@ print("How to use playgrounds to make writing Swift fun and simple")
 /*:
  Now print your own phrases to the console. Pick one of your favorite songs. Use your knowledge of the `print` function to display the song title and artist.
  */
-
+print("Favorite Song Titled: ")
+print("Waymaker")
+print("Artist: ")
+print("Sinache")
 
 /*:
  Use multiple `print` functions to write out some of the lyrics to the song.
  */
-
-
+print("YOU are here moving in our midst")
+print(" I worship YOU ")
+print("I worship YOU...")
+print(YOU are, Waymaker, Miracle Worker, Promise Keeper, Light in the Darkness, My GOD, that is who YOU are")
 
 /*:
 
