a1cb883edc7e3dc56d4b18a1c3567df97431af00
SJ2523 <lees2523@durhamtech.edu>
2025-01-26 22:05:53 -0500

    Update Contents.swift

--- Unit1/Lesson1_3/Lab_Constants_Variables.playground/Contents.swift
+++ Unit1/Lesson1_3/Lab_Constants_Variables.playground/Contents.swift
@@ -3,17 +3,18 @@
  
  Declare a constant called `friends` to represent the number of friends you have on social media. Give it a value between 50 and 1000. Print out the value by referencing your constant.
  */
-
+let friend = 353
+print(friends)
 
 /*:
  Now assume you go through and remove a lot of your friends that aren't active on social media. Update your `friends` constant to a lower number than it currently is between 1 and 900.
  */
-
+friends = 5
 
 /*:
  Does the above code compile? Why not? Print your explanation to the console using the `print` function. Go back and delete your line of code that updates the `friend` constant to a lower number so that the playground will compile properly.
  */
-
+print("constants are immutable")
 
 /*:
  Declare a variable `age` and set it to your own age. Print `age` to the console.
