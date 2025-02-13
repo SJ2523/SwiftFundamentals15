051c0b0f932356a34ac14197a3490ef5e2c77d8f
SJ2523 <lees2523@durhamtech.edu>
2025-02-12 20:48:58 -0500

    Started Lesson 1_3

--- Unit1/Lesson1_3/Lab_Constants_Variables.playground/Pages/3. Exercise - Variables.xcplaygroundpage/Contents.swift
+++ Unit1/Lesson1_3/Lab_Constants_Variables.playground/Pages/3. Exercise - Variables.xcplaygroundpage/Contents.swift
@@ -1,8 +1,3 @@
-/*:
-## Exercise - Variables
- 
- Declare a variable `schooling` and set it to the number of years of school that you have completed. Print `schooling` to the console.
- */
 
 
 //:  Now imagine you just completed an additional year of school, and update the `schooling` variable accordingly. Print `schooling` to the console.
