2078c777e046900ce1ebbf32cc4ec438f96d0d88
SJ2523 <lees2523@durhamtech.edu>
2025-02-16 12:38:48 -0500

    Started Unit 2/Lesson 2_2 - Strings

--- Unit2/Lesson2_2/Lab_Strings.playground/Pages/2. Exercise - Concatenation and Interpolation.xcplaygroundpage/Contents.swift
+++ Unit2/Lesson2_2/Lab_Strings.playground/Pages/2. Exercise - Concatenation and Interpolation.xcplaygroundpage/Contents.swift
@@ -3,8 +3,10 @@
  
  Create a `city` constant and assign it a string literal representing your home city. Then create a `state` constant and assign it a string literal representing your home state. Finally, create a `home` constant and use string concatenation to assign it a string representing your home city and state (i.e. Portland, Oregon). Print the value of `home`.
  */
-
-
+let city: String = "Brooklyn"
+let state: String = "New York"
+let home = city + " " + state
+print(home)
 //:  Use the compound assignment operator (`+=`) to add `home` to `introduction` below. Print the value of `introduction`.
 var introduction = "I live in "
 
