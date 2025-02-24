c1e51657a1cbcf0557744d73d7f197262e253442
SJ2523 <lees2523@durhamtech.edu>
2025-02-23 22:31:25 -0500

    Completed Unit 2/Lesson 2_5

--- Unit2/Lesson2_5/Lab_Classes.playground/Pages/3. Exercise - Override Methods and Properties.xcplaygroundpage/Contents.swift
+++ Unit2/Lesson2_5/Lab_Classes.playground/Pages/3. Exercise - Override Methods and Properties.xcplaygroundpage/Contents.swift
@@ -28,6 +28,11 @@ class Fighter: Spaceship {
     var weapon = ""
     var remainingFirePower = 5
     
+    init(weapon: String = "", remainingFirePower: Int = 5) {
+        self.weapon = weapon
+        self.remainingFirePower = remainingFirePower
+    }
+    
     func fire() {
         if remainingFirePower > 0 {
             remainingFirePower -= 1
@@ -37,14 +42,38 @@ class Fighter: Spaceship {
     }
 }
 //:  Define a new class `ShieldedShip` that inherits from `Fighter`. Add a variable property `shieldStrength` that defaults to 25. Create a new instance of `ShieldedShip` called `defender`. Set `name` to "Defender" and `weapon` to "Cannon." Call `moveRight()` and print `position`, then call `fire()` and print `remainingFirePower`.
+class ShieldedShip: Fighter {
+    
+    var shieldStrength: Int = 25
+    
+    override func wasHit() {
+        if shieldStrength > 0 {
+            shieldStrength -= 5
+        } else{
+                health -= 5
+        }
+    }
+    
+}
+let defender = ShieldedShip()
+    
+defender.moveRight()
 
 
+print(defender.remainingFirePower)
+
+defender.wasHit()
+
+print (defender.shieldStrength)
+print (defender.health)
+
+print(defender.remainingFirePower)
 //:  Go back to your declaration of `ShieldedShip` and override `wasHit()`. In the body of the method, check to see if `shieldStrength` is greater than 0. If it is, decrement `shieldStrength` by 5. Otherwise, decrement `health` by 5. Call `wasHit()` on `defender` and print `shieldStrength` and `health`.
+defender.wasHit()
 
-
+print(defender.shieldStrength)
+print(defender.health)
 //:  When `shieldStrength` is 0, all `wasHit()` does is decrement `health` by 5. That's exactly what the implementation of `wasHit()` on `Spaceship` does! Instead of rewriting that, you can call through to the superclass implementation of `wasHit()`. Go back to your implementation of `wasHit()` on `ShieldedShip` and remove the code where you decrement `health` by 5 and replace it with a call to the superclass's implementation of the method. Call `wasHit()` on `defender`, then print `shieldStrength` and `health`.
+print(defender.shieldStrength)
 
 
-/*:
-[Previous](@previous)  |  page 3 of 4  |  [Next: Exercise - Class Memberwise Initializers and References](@next)
- */
